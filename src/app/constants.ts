f213120bc20a1c63b1a9460155039891b3efe90e
avazbek <avazbekdevleoper11@gmail.com>
2026-04-09 18:15:01 +0500

    change backend url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = typeof window !== 'undefined' 
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || (typeof window !== 'undefined' 
   ? `http://${window.location.hostname}:3001` 
-  : 'http://localhost:3001';
+  : 'http://localhost:3001');
